7c68a1358ef820b073171b2c38fd080d0aa70003
Wojciech Hejna <wojciech.hejna@gft.com>
2017-02-23 15:48:34 +0100

    first commit

---
+++ src/main/java/com/wojtekhejna/web3j_example/App.java
@@ -0,0 +1,9 @@
+package com.wojtekhejna.web3j_example;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/com/wojtekhejna/web3j_example/AppTest.java
@@ -0,0 +1,12 @@
+package com.wojtekhejna.web3j_example;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
