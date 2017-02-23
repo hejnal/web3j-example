d1a2ae06da57915ac6cde89f3eda1ae93ee28408
Wojciech Hejna <wojciech.hejna@gft.com>
2017-02-23 19:36:47 +0100

    add the web3j dependency

--- src/main/java/com/wojtekhejna/web3j_example/App.java
+++ src/main/java/com/wojtekhejna/web3j_example/App.java
@@ -1,9 +1,23 @@
 package com.wojtekhejna.web3j_example;
 
+import java.util.concurrent.ExecutionException;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		System.out.println("Hello World!");
+
+		Web3j web3 = Web3j.build(new HttpService());
+
+		Web3ClientVersion clientVersion = web3.web3ClientVersion().sendAsync().get();
+
+		if (!clientVersion.hasError()) {
+			System.out.println("Client is running version: " + clientVersion.getWeb3ClientVersion());
+		}
+	}
 
 }
--- src/test/java/com/wojtekhejna/web3j_example/AppTest.java
+++ src/test/java/com/wojtekhejna/web3j_example/AppTest.java
@@ -1,11 +1,13 @@
 package com.wojtekhejna.web3j_example;
 
+import java.util.concurrent.ExecutionException;
+
 import org.junit.Test;
 
 public class AppTest {
 
   @Test
-  public void testMain() {
+  public void testMain() throws InterruptedException, ExecutionException {
     App.main(null);
   }
 
